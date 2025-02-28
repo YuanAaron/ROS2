256a2e34ee69e9ecb61c25a7973efdf51b0f8c3d
oshacker <1500438364@qq.com>
2025-02-28 15:05:22 +0800

    cpp helloworld

--- 01/src/01pkg_helloworld_cpp/src/helloworld.cpp
+++ 01/src/01pkg_helloworld_cpp/src/helloworld.cpp
@@ -1,10 +1,14 @@
-#include <cstdio>
+#include "rclcpp/rclcpp.hpp"
 
 int main(int argc, char ** argv)
 {
-  (void) argc;
-  (void) argv;
-
-  printf("hello world 01pkg_helloworld_cpp package\n");
-  return 0;
+	//初始化ros2
+	rclcpp::init(argc, argv);
+	//创建节点
+	auto node = rclcpp::Node::make_shared("helloworld_node_cpp");
+	//输出文本
+	RCLCPP_INFO(node -> get_logger(), "hello world");
+	//释放资源
+	rclcpp::shutdown();
+  	return 0;
 }
